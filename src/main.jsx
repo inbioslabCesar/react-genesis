0c3babcdb6b9e359afcc4f7e44265990dec2b899
cesarperezguimaraez <inbioslab@gmail.com>
2023-04-26 08:20:35 -0500

    config

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const app = <img src='/logo.svg' />;
+const app = (
+	<div>
+		<div>
+			<span>Juan Perez</span>
+			<span>Profesor</span>
+		</div>
+	</div>
+);
 const container = document.getElementById('root');
 
 ReactDOM.render(app, container);
